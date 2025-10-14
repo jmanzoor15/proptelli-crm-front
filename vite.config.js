b815af1fc47dbd8d4c4d1af35ef6dd99b07189c3
junaid <jmanzoor15@gmail.com>
2025-10-14 12:21:38 +0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Important for deployment
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://api.crm.proptelli.com',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 })
